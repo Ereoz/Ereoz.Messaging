ee652b174409a1ca50d2f4b088aba1809f0757ac
Denis Plotnikov <denereoz@gmail.com>
2025-10-28 12:48:33 +0700

    Initial commit

---
+++ Ereoz.Messaging/Messenger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ereoz.Abstractions.Messaging;
+
+namespace Ereoz.Messaging
+{
+    /// <summary>
+    /// Мессенджер для реализации событийной модели межкомпонентного взаимодействия.
+    /// </summary>
+    public class Messenger : IMessenger
+    {
+        private static class TypedEvent<T>
+        {
+            public static event Action<T> Event;
+
+            public static void Send(T message) =>
+                Event?.Invoke(message);
+        }
+
+        /// <inheritdoc/>
+        public void Subscribe<T>(Action<T> handler) =>
+            TypedEvent<T>.Event += handler;
+
+        /// <inheritdoc/>
+        public void Unsubscribe<T>(Action<T> handler) =>
+            TypedEvent<T>.Event -= handler;
+
+        /// <inheritdoc/>
+        public void Send<T>(T message) =>
+            TypedEvent<T>.Send(message);
+    }
+}
